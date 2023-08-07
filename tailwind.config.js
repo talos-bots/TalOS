1554a25145b0d4456dd0330f8068de378be0c7fb
thesentinel2615 <nevyn202@outlook.com>
2023-08-07 18:36:40 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,86 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        '5vw': '5vw',
+        '10vw': '10vw',
+        '15vw': '15vw',
+        '20vw': '20vw',
+        '25vw': '25vw',
+        '30vw': '30vw',
+        '35vw': '35vw',
+        '40vw': '40vw',
+        '45vw': '45vw',
+        '50vw': '50vw',
+        '55vw': '55vw',
+        '60vw': '60vw',
+        '65vw': '65vw',
+        '70vw': '70vw',
+        '75vw': '75vw',
+        '80vw': '80vw',
+        '85vw': '85vw',
+        '90vw': '90vw',
+        '95vw': '95vw',
+        '100vw': '100vw',
+      },
+      height: {
+        '5vh': '5vh',
+        '10vh': '10vh',
+        '15vh': '15vh',
+        '20vh': '20vh',
+        '25vh': '25vh',
+        '30vh': '30vh',
+        '35vh': '35vh',
+        '40vh': '40vh',
+        '45vh': '45vh',
+        '50vh': '50vh',
+        '55vh': '55vh',
+        '60vh': '60vh',
+        '65vh': '65vh',
+        '75vh': '75vh',
+        '80vh': '80vh',
+        '85vh': '85vh',
+        '90vh': '90vh',
+        '95vh': '95vh',
+        '100vh': '100vh',
+      },
+      backgroundColor: {
+        'theme-root': 'var(--theme-root)',
+        'theme-italic': 'var(--theme-italic)',
+        'theme-text': 'var(--theme-text)',
+        'theme-box': 'var(--theme-box)',
+        'theme-hover-pos': 'var(--theme-hover-pos)',
+        'theme-hover-neg': 'var(--theme-hover-neg)',
+      },
+      textColor: {
+        'theme-text': 'var(--theme-text)',
+        'theme-italic': 'var(--theme-italic)',
+        'theme-text-hover': 'var(--theme-text-hover)',
+      },
+      borderColor: {
+        'theme-border': 'var(--theme-border)',
+      },
+      backdropBlur: {
+        'theme-blur': 'var(--theme-blur)',
+      },
+      borderWidth: {
+        'theme-border-width': 'var(--theme-border-width)',
+      },
+      borderRadius: {
+        'theme-border-radius': 'var(--theme-border-radius)',
+      },
+    },
+  },
+  variants: {
+      extend: {
+        backdropBlur: ['responsive'],
+      },
+  },
+  plugins: [
+    require('tailwindcss-textshadow'),
+  ],
+};
