248e7314a7e3a34f07df3aa15c76731e57ab3a3c
thesentinel2615 <nevyn202@outlook.com>
2023-08-07 20:03:48 -0500

    port in DiscordBot Client

--- electron/main/discord.ts
+++ electron/main/discord.ts
@@ -1,5 +1,23 @@
 import { ipcMain } from 'electron';
+import { ActivityType, Client, GatewayIntentBits, Collection, REST, Routes, Partials } from 'discord.js';
+
+const intents = { 
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.MessageContent, GatewayIntentBits.GuildEmojisAndStickers, 
+    GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildModeration], 
+    partials: [Partials.Channel, Partials.GuildMember, Partials.User, Partials.Reaction, Partials.Message] 
+};
 
 export function DiscordJSRoutes(){
+    const disClient = new Client(intents);
+    const commands = new Collection();
 
+    disClient.on('ready', () => {
+        if(!disClient.user) return;
+        if(disClient.user){
+            disClient.user.setActivity({ name: 'with your feelings', type: ActivityType.Playing });
+        }
+        console.log(`Logged in as ${disClient.user.tag}!`);
+    });
 };
