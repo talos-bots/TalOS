7fb306faf3d10986464b1dfd1fb2ee0fd868f31d
thesentinel2615 <nevyn202@outlook.com>
2023-08-08 15:43:57 -0500

    Default Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { useState } from 'react'
 import './App.scss'
 import { ConstructOS } from './components/constructOS';
 import { DiscordListeners } from './listeners/discord-listeners';
+import AgentsPage from './pages/agents';
+import ActionsPage from './pages/actions';
+import SettingsPage from './pages/settings';
+import DocsPage from './pages/docs';
 
 console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
 
@@ -14,6 +18,10 @@ function App() {
       <Routes>
         <Route path='/*' element={<></>} />
         <Route path='/terminal' element={<ConstructOS/>} />
+        <Route path='/agents' element={<AgentsPage/>} />
+        <Route path='/actions' element={<ActionsPage/>} />
+        <Route path='/settings' element={<SettingsPage/>} />
+        <Route path='/docs' element={<DocsPage/>} />
       </Routes>
   </Router>
   </div>
---
+++ src/pages/actions/index.tsx
@@ -0,0 +1,10 @@
+const ActionsPage = () => {
+
+    return (
+        <div>
+            <h1>Actions</h1>
+        </div>
+    );
+}
+
+export default ActionsPage;
