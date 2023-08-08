cf9ff95b7d1eda26f271a82530c46ac599a4eab6
thesentinel2615 <nevyn202@outlook.com>
2023-08-07 20:10:50 -0500

    discord functions.

--- electron/main/discord.ts
+++ electron/main/discord.ts
@@ -9,6 +9,21 @@ const intents = {
     partials: [Partials.Channel, Partials.GuildMember, Partials.User, Partials.Reaction, Partials.Message] 
 };
 
+function cleanUsername(username: string) {
+    // Remove leading characters
+    let cleaned = username.replace(/^[._-]+/, '');
+  
+    // Remove trailing characters
+    cleaned = cleaned.replace(/[._-]+$/, '');
+  
+    return cleaned;
+}
+
+function cleanEmoji(text: string) {
+    // Remove emoji characters using regex
+    return text.replace(/<:[a-zA-Z0-9_]+:[0-9]+>/g, '');
+}
+
 export function DiscordJSRoutes(){
     const disClient = new Client(intents);
     const commands = new Collection();
@@ -20,4 +35,31 @@ export function DiscordJSRoutes(){
         }
         console.log(`Logged in as ${disClient.user.tag}!`);
     });
+
+    async function setStatus(message: string, type: string){
+        if(!disClient.user) return;
+        let activityType;
+        switch (type) {
+            case 'Playing':
+            activityType = ActivityType.Playing;
+            break;
+            case 'Watching':
+            activityType = ActivityType.Watching;
+            break;
+            case 'Listening':
+            activityType = ActivityType.Listening;
+            break;
+            case 'Streaming':
+            activityType = ActivityType.Streaming;
+            break;
+            case 'Competing':
+            activityType = ActivityType.Competing;
+            break;
+            default:
+            activityType = ActivityType.Playing;
+            break;
+        }
+        if(!activityType) return;
+        disClient.user.setActivity(`${message}`, {type: activityType});
+    }
 };
