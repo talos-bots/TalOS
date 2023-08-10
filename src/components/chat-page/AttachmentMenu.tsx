7b55bbbaa50d7df92c9eeb7fa1cb5af0e2c8fa89
Otis Alejandro <128675915+OtisAlejandro@users.noreply.github.com>
2023-08-09 19:27:34 -0500

    hover animation

--- src/components/chat-page/AttachmentMenu.tsx
+++ src/components/chat-page/AttachmentMenu.tsx
@@ -10,21 +10,21 @@ const AttachmentMenu: React.FC<AttachmentMenuProps> = ({ isOpen }) => {
   return (
     <div className="attachment-containter">
       <div className={`w-[35px] h-[177px] flex flex-col pt-[52px] px-0 pb-0 box-border items-center justify-start gap-[10px] ml-[.73%] mb-[65px] ${attachmentMenuClass}`}>
-      <button className={`cursor-pointer border-none p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0`}>
+      <button className={`cursor-pointer border-none p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0 transition-all duration-125 hover:opacity-75`}>
           <img
             className="absolute top-[10px] left-[10px] w-[15px] h-[15px] overflow-hidden"
             alt=""
             src="/chat-page-assets/file.svg"
           />
         </button>
-        <button className={`cursor-pointer [border:none] p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0`}>
+        <button className={`cursor-pointer [border:none] p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0 transition-all duration-125 hover:opacity-75`}>
           <img
             className="absolute top-[10px] left-[10px] w-[15px] h-[15px] overflow-hidden"
             alt=""
             src="/chat-page-assets/code.svg"
           />
         </button>
-        <button className={`cursor-pointer [border:none] p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0`}>
+        <button className={`cursor-pointer [border:none] p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0 transition-all duration-125 hover:opacity-75`}>
           <img
             className="absolute top-[10px] left-[10px] w-[15px] h-[15px] overflow-hidden"
             alt=""
--- src/components/chat-page/ChatInput.tsx
+++ src/components/chat-page/ChatInput.tsx
@@ -12,7 +12,7 @@ const InputGroup: React.FC = () => {
   return (
     <div>
       <div className={`rounded-full bg-black bg-opacity-25 backdrop-blur-sm box-border w-[1151px] h-[51px] overflow-hidden shrink-0 flex flex-row py-0 px-2.5 items-center justify-start gap-[23px] border-[1px] border-solid border-lime-200`}>
-        <button className={`cursor-pointer p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0 ${attachmentMenuOpen ? "attachment-menu-open" : ""}`} onClick={toggleAttachmentMenu}>
+        <button className={`cursor-pointer p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0 transition-all duration-125 hover:opacity-75 ${attachmentMenuOpen ? "attachment-menu-open" : ""}`} onClick={toggleAttachmentMenu}>
           <img className="absolute top-[10px] left-[10px] w-[15px] h-[15px] overflow-hidden" alt="" src="/chat-page-assets/pluscircle.svg" />
         </button>
         <input
@@ -21,7 +21,7 @@ const InputGroup: React.FC = () => {
           placeholder="Type your message..."
           required
         />
-        <button className={`cursor-pointer border-none p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0`}>
+        <button className={`cursor-pointer border-none p-0 bg-lime-200 relative rounded-3xl w-[35px] h-[35px] overflow-hidden shrink-0 transition-all duration-125 hover:opacity-75`}>
           <img className="absolute top-[10px] left-[10px] w-[15px] h-[15px] overflow-hidden" alt="" src="/chat-page-assets/sendhorizonal.svg" />
         </button>
       </div>
