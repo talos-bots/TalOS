90741ac5018d6426e2cae65c38e46fc3f6053188
thesentinel2615 <nevyn202@outlook.com>
2023-08-12 19:05:12 -0500

    appwide backbut

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import DevPanel from './components/dev-panel';
 import NavBar from './components/shared/NavBar';
 import SettingsPage from './pages/settings';
 import AgentManagement from './components/agent-crud';
+import { useEffect } from 'react';
 
 function App() {
   const greenTheme = {
@@ -18,7 +19,19 @@ function App() {
   };
 
   const selectedTheme = greenTheme;
+  
+  const returnToMenu = () => {
+    history.back();
+  }
 
+  useEffect(() => {
+      const closeOnEscapeKey = (e: { key: string; }) => e.key === "Escape" ? returnToMenu() : null;
+      document.body.addEventListener("keydown", closeOnEscapeKey);
+      return () => {
+          document.body.removeEventListener("keydown", closeOnEscapeKey);
+      };
+  }, []);
+  
   const isDev = process.env.NODE_ENV === 'development';
   return (
     <div id='App'>
