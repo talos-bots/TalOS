f49e80116f89cd2faa30692e7849226aff67c79e
thesentinel2615 <nevyn202@outlook.com>
2023-08-13 15:27:03 -0500

    discord main process functions

--- electron/main/api/discord.ts
+++ electron/main/api/discord.ts
@@ -10,7 +10,13 @@ const intents = {
 };
 type ValidStatus = 'online' | 'dnd' | 'idle' | 'invisible';
 
-function cleanUsername(username: string) {
+let disClient = new Client(intents);
+const commands = new Collection();
+let isReady = false;
+let token = '';
+let applicationID = '';
+
+export function cleanUsername(username: string) {
     // Remove leading characters
     let cleaned = username.replace(/^[._-]+/, '');
   
@@ -20,18 +26,158 @@ function cleanUsername(username: string) {
     return cleaned;
 }
 
-function cleanEmoji(text: string) {
+export function cleanEmoji(text: string) {
     // Remove emoji characters using regex
     return text.replace(/<:[a-zA-Z0-9_]+:[0-9]+>/g, '');
 }
 
-export function DiscordJSRoutes(){
-    let disClient = new Client(intents);
-    const commands = new Collection();
-    let isReady = false;
-    let token = '';
-    let applicationID = '';
+export async function setDiscordBotInfo(botName: string, base64Avatar: string): Promise<void> {
+    if(!isReady) return;
+    if (!disClient.user) {
+        console.error("Discord client user is not initialized.");
+        return;
+    }
+    let newName;
+    let newNameDot;
+    try {
+        await disClient.user.setUsername(botName);
+        console.log(`My new username is ${botName}`);
+    } catch (error) {
+        console.error(`Failed to set username to ${botName}:`, error);
 
+        // If the first attempt fails, add an underscore and try again
+        try {
+            newName = "_" + botName;
+            await disClient.user.setUsername(newName);
+            console.log(`My new username is ${newName}`);
+        } catch (error) {
+            console.error(`Failed to set username to ${newName}:`, error);
+
+            // If the second attempt fails, add a dot and try again
+            try {
+                newNameDot = "." + botName;
+                await disClient.user.setUsername(newNameDot);
+                console.log(`My new username is ${newNameDot}`);
+            } catch (error) {
+                console.error(`Failed to set username to ${newNameDot}:`, error);
+            }
+        }
+    }
+
+    // Change bot's avatar
+    try {
+        const buffer = Buffer.from(base64Avatar, 'base64');
+        await disClient.user.setAvatar(buffer);
+        console.log('New avatar set!');
+    } catch (error) {
+        console.error('Failed to set avatar:', error);
+    }
+}
+
+export async function getDiscordGuilds() {
+    if(!isReady) return false;
+    const guilds = disClient.guilds.cache.map(guild => {
+        const channels = guild.channels.cache
+          .filter(channel => channel.type === 0)
+          .map(channel => ({
+            id: channel.id,
+            name: channel.name,
+          }));
+        return {
+          id: guild.id,
+          name: guild.name,
+          channels,
+        };
+    });
+    return guilds;
+}
+
+export async function setStatus(message: string, type: string){
+    if(!disClient.user) return;
+    if(!isReady) return;
+
+    let activityType: ActivityType.Playing | ActivityType.Streaming | ActivityType.Listening | ActivityType.Watching | ActivityType.Competing;
+
+    switch (type) {
+        case 'Playing':
+            activityType = ActivityType.Playing;
+            break;
+        case 'Watching':
+            activityType = ActivityType.Watching;
+            break;
+        case 'Listening':
+            activityType = ActivityType.Listening;
+            break;
+        case 'Streaming':
+            activityType = ActivityType.Streaming;
+            break;
+        case 'Competing':
+            activityType = ActivityType.Competing;
+            break;
+        default:
+            activityType = ActivityType.Playing;
+            break;
+    }
+
+    disClient.user.setActivity(`${message}`, {type: activityType});
+}
+
+export async function setOnlineMode(type: ValidStatus) {
+    if(!disClient.user) return;
+    if(!isReady) return;
+    disClient.user.setStatus(type);
+}
+
+export async function sendMessage(channelID: Snowflake, message: string): Promise<void> {
+    if(!isReady) return;
+    if (!disClient.user) {
+        console.error("Discord client user is not initialized.");
+        return;
+    }
+    const channel = await disClient.channels.fetch(channelID);
+
+    // Check if the channel is one of the types that can send messages
+    if (channel instanceof TextChannel || channel instanceof DMChannel || channel instanceof NewsChannel) {
+        channel.send(message);
+    }
+}
+
+export async function getWebhookForCharacter(charName: string, channelID: Snowflake): Promise<Webhook | undefined> {
+    if(!isReady) return;
+    const channel = disClient.channels.cache.get(channelID);
+
+    if (!(channel instanceof TextChannel || channel instanceof NewsChannel)) {
+        return undefined;
+    }
+
+    const webhooks = await channel.fetchWebhooks();
+    return webhooks.find(webhook => webhook.name === charName);
+}
+
+export async function sendMessageAsCharacter(charName: string, channelID: Snowflake, message: string): Promise<void> {
+    if(!isReady) return;
+    const webhook = await getWebhookForCharacter(charName, channelID);
+    
+    if (!webhook) {
+        throw new Error(`Webhook for character ${charName} not found.`);
+    }
+
+    await webhook.send(message);
+}
+
+export async function getWebhooksForChannel(channelID: Snowflake): Promise<string[]> {
+    if(!isReady) return [];
+    const channel = disClient.channels.cache.get(channelID);
+
+    if (!(channel instanceof TextChannel || channel instanceof NewsChannel)) {
+        return [];
+    }
+
+    const webhooks = await channel.fetchWebhooks();
+    return webhooks.map(webhook => webhook.name);
+}
+
+export function DiscordJSRoutes(){
     ipcMain.on('discord-get-token', async (event) => {
         event.sender.send('discord-get-token-reply', token);
     });
@@ -64,21 +210,7 @@ export function DiscordJSRoutes(){
     });
 
     ipcMain.on('discord-get-guilds', async (event) => {
-        if(!isReady) return false;
-        const guilds = disClient.guilds.cache.map(guild => {
-            const channels = guild.channels.cache
-              .filter(channel => channel.type === 0)
-              .map(channel => ({
-                id: channel.id,
-                name: channel.name,
-              }));
-            return {
-              id: guild.id,
-              name: guild.name,
-              channels,
-            };
-        });
-        event.sender.send('discord-get-guilds-reply', guilds);
+        event.sender.send('discord-get-guilds-reply', await getDiscordGuilds());
     });
 
     disClient.on('messageCreate', async (message) => {
@@ -213,134 +345,6 @@ export function DiscordJSRoutes(){
         console.log(`Logged in as ${disClient.user.tag}!`);
         ipcMain.emit('discord-ready', disClient);
     });
-
-    async function setDiscordBotInfo(botName: string, base64Avatar: string): Promise<void> {
-        if(!isReady) return;
-        if (!disClient.user) {
-            console.error("Discord client user is not initialized.");
-            return;
-        }
-        let newName;
-        let newNameDot;
-        try {
-            await disClient.user.setUsername(botName);
-            console.log(`My new username is ${botName}`);
-        } catch (error) {
-            console.error(`Failed to set username to ${botName}:`, error);
-
-            // If the first attempt fails, add an underscore and try again
-            try {
-                newName = "_" + botName;
-                await disClient.user.setUsername(newName);
-                console.log(`My new username is ${newName}`);
-            } catch (error) {
-                console.error(`Failed to set username to ${newName}:`, error);
-    
-                // If the second attempt fails, add a dot and try again
-                try {
-                    newNameDot = "." + botName;
-                    await disClient.user.setUsername(newNameDot);
-                    console.log(`My new username is ${newNameDot}`);
-                } catch (error) {
-                    console.error(`Failed to set username to ${newNameDot}:`, error);
-                }
-            }
-        }
-    
-        // Change bot's avatar
-        try {
-            const buffer = Buffer.from(base64Avatar, 'base64');
-            await disClient.user.setAvatar(buffer);
-            console.log('New avatar set!');
-        } catch (error) {
-            console.error('Failed to set avatar:', error);
-        }
-    }
-
-    async function setStatus(message: string, type: string){
-        if(!disClient.user) return;
-        if(!isReady) return;
-    
-        let activityType: ActivityType.Playing | ActivityType.Streaming | ActivityType.Listening | ActivityType.Watching | ActivityType.Competing;
-    
-        switch (type) {
-            case 'Playing':
-                activityType = ActivityType.Playing;
-                break;
-            case 'Watching':
-                activityType = ActivityType.Watching;
-                break;
-            case 'Listening':
-                activityType = ActivityType.Listening;
-                break;
-            case 'Streaming':
-                activityType = ActivityType.Streaming;
-                break;
-            case 'Competing':
-                activityType = ActivityType.Competing;
-                break;
-            default:
-                activityType = ActivityType.Playing;
-                break;
-        }
-    
-        disClient.user.setActivity(`${message}`, {type: activityType});
-    }
-
-    async function setOnlineMode(type: ValidStatus) {
-        if(!disClient.user) return;
-        if(!isReady) return;
-        disClient.user.setStatus(type);
-    }
-    
-    async function sendMessage(channelID: Snowflake, message: string): Promise<void> {
-        if(!isReady) return;
-        if (!disClient.user) {
-            console.error("Discord client user is not initialized.");
-            return;
-        }
-        const channel = await disClient.channels.fetch(channelID);
-    
-        // Check if the channel is one of the types that can send messages
-        if (channel instanceof TextChannel || channel instanceof DMChannel || channel instanceof NewsChannel) {
-            channel.send(message);
-        }
-    }
-
-    async function getWebhookForCharacter(charName: string, channelID: Snowflake): Promise<Webhook | undefined> {
-        if(!isReady) return;
-        const channel = disClient.channels.cache.get(channelID);
-    
-        if (!(channel instanceof TextChannel || channel instanceof NewsChannel)) {
-            return undefined;
-        }
-    
-        const webhooks = await channel.fetchWebhooks();
-        return webhooks.find(webhook => webhook.name === charName);
-    }
-    
-    async function sendMessageAsCharacter(charName: string, channelID: Snowflake, message: string): Promise<void> {
-        if(!isReady) return;
-        const webhook = await getWebhookForCharacter(charName, channelID);
-        
-        if (!webhook) {
-            throw new Error(`Webhook for character ${charName} not found.`);
-        }
-    
-        await webhook.send(message);
-    }
-    
-    async function getWebhooksForChannel(channelID: Snowflake): Promise<string[]> {
-        if(!isReady) return [];
-        const channel = disClient.channels.cache.get(channelID);
-    
-        if (!(channel instanceof TextChannel || channel instanceof NewsChannel)) {
-            return [];
-        }
-    
-        const webhooks = await channel.fetchWebhooks();
-        return webhooks.map(webhook => webhook.name);
-    }
 
     ipcMain.handle('discord-login', async (event, rawToken: string, appId: string) => {
         await disClient.login(rawToken);
