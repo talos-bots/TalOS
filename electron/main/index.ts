ed83f53fcd8e3d2db06fcda0a08b29c2a59c18e0
ausboss <102309304+ausboss@users.noreply.github.com>
2023-08-13 20:34:12 -0500

    better config file code

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,16 +1,16 @@
-import { app, BrowserWindow, shell, ipcMain } from 'electron'
-import { release } from 'node:os'
-import { join } from 'node:path'
-import path from 'path';
-import { DiscordJSRoutes } from './api/discord'
-import { PouchDBRoutes } from './api/pouchdb'
-import Store from 'electron-store';
-import { FsAPIRoutes } from './api/fsapi';
-import { LanguageModelAPI } from './api/llm';
-import { SDRoutes } from './api/sd';
-import { BonusFeaturesRoutes } from './api/bonus-features';
-import constructController from './controllers/ConstructController';
-import fs from 'fs';
+import { app, BrowserWindow, shell, ipcMain } from "electron";
+import { release } from "node:os";
+import { join } from "node:path";
+import path from "path";
+import { DiscordJSRoutes } from "./api/discord";
+import { PouchDBRoutes } from "./api/pouchdb";
+import Store from "electron-store";
+import { FsAPIRoutes } from "./api/fsapi";
+import { LanguageModelAPI } from "./api/llm";
+import { SDRoutes } from "./api/sd";
+import { BonusFeaturesRoutes } from "./api/bonus-features";
+import constructController from "./controllers/ConstructController";
+import fs from "fs";
 
 // The built directory structure
 //
@@ -22,39 +22,39 @@ import fs from 'fs';
 // ├─┬ dist
 // │ └── index.html    > Electron-Renderer
 //
-process.env.DIST_ELECTRON = join(__dirname, '../')
-process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
+process.env.DIST_ELECTRON = join(__dirname, "../");
+process.env.DIST = join(process.env.DIST_ELECTRON, "../dist");
 process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? join(process.env.DIST_ELECTRON, '../public')
-  : process.env.DIST
+  ? join(process.env.DIST_ELECTRON, "../public")
+  : process.env.DIST;
 
 // Disable GPU Acceleration for Windows 7
-if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+if (release().startsWith("6.1")) app.disableHardwareAcceleration();
 
 // Set application name for Windows 10+ notifications
-if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+if (process.platform === "win32") app.setAppUserModelId(app.getName());
 
 if (!app.requestSingleInstanceLock()) {
-  app.quit()
-  process.exit(0)
+  app.quit();
+  process.exit(0);
 }
 
 // Remove electron security warnings
 // This warning only shows in development mode
 // Read more on https://www.electronjs.org/docs/latest/tutorial/security
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 
-let win: BrowserWindow | null = null
+let win: BrowserWindow | null = null;
 // Here, you can also use other preload
-const preload = join(__dirname, '../preload/index.js')
-const url = process.env.VITE_DEV_SERVER_URL
-const indexHtml = join(process.env.DIST, 'index.html')
-export const dataPath = path.join(app.getPath('userData'), 'data/');
+const preload = join(__dirname, "../preload/index.js");
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = join(process.env.DIST, "index.html");
+export const dataPath = path.join(app.getPath("userData"), "data/");
 export const store = new Store();
 async function createWindow() {
   win = new BrowserWindow({
-    title: 'ConstructOS - AI Agent Manager',
-    icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
+    title: "ConstructOS - AI Agent Manager",
+    icon: join(process.env.VITE_PUBLIC, "favicon.ico"),
     webPreferences: {
       preload,
       nodeIntegration: true,
@@ -68,20 +68,19 @@ async function createWindow() {
     resizable: true,
     maximizable: true,
     minimizable: false,
-  })
+  });
 
   if (url) {
-    win.loadURL(url)
-    win.webContents.openDevTools()
+    win.loadURL(url);
+    win.webContents.openDevTools();
   } else {
-    win.loadFile(indexHtml)
+    win.loadFile(indexHtml);
   }
 
   win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url)
-    return { action: 'deny' }
-  })
-
+    if (url.startsWith("https:")) shell.openExternal(url);
+    return { action: "deny" };
+  });
 
   DiscordJSRoutes();
   PouchDBRoutes();
@@ -93,71 +92,88 @@ async function createWindow() {
   // update(win)
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  win = null
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('second-instance', () => {
+app.on("second-instance", () => {
   if (win) {
     // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore()
-    win.focus()
+    if (win.isMinimized()) win.restore();
+    win.focus();
   }
-})
+});
 
-app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows()
+app.on("activate", () => {
+  const allWindows = BrowserWindow.getAllWindows();
   if (allWindows.length) {
-    allWindows[0].focus()
+    allWindows[0].focus();
   } else {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-app.on('ready', () => {
-  const { session } = require('electron');
+app.on("ready", () => {
+  const { session } = require("electron");
   session.defaultSession.clearCache();
 });
 // New window example arg: new windows url
-ipcMain.handle('open-win', (_, arg) => {
+ipcMain.handle("open-win", (_, arg) => {
   const childWindow = new BrowserWindow({
     webPreferences: {
       preload,
       nodeIntegration: true,
       contextIsolation: false,
     },
-  })
+  });
 
   if (process.env.VITE_DEV_SERVER_URL) {
-    childWindow.loadURL(`${url}#${arg}`)
+    childWindow.loadURL(`${url}#${arg}`);
   } else {
-    childWindow.loadFile(indexHtml, { hash: arg })
+    childWindow.loadFile(indexHtml, { hash: arg });
   }
-})
+});
 
-ipcMain.handle('get-data-path', () => {
+ipcMain.handle("get-data-path", () => {
   return dataPath;
-})
+});
 
-ipcMain.on('set-data', (event, arg) => {
+ipcMain.on("set-data", (event, arg) => {
   store.set(arg.key, arg.value);
-})
+});
 
-ipcMain.on('get-data', (event, arg) => {
-  event.sender.send('get-data-reply', store.get(arg));
-})
+ipcMain.on("get-data", (event, arg) => {
+  event.sender.send("get-data-reply", store.get(arg));
+});
 
-ipcMain.handle('get-server-port', (event) => {
+ipcMain.handle("get-server-port", (event) => {
   try {
     const configPath = path.join(__dirname, "backend", "config.json");
-    const rawData = fs.readFileSync(configPath, 'utf8');
+    const rawData = fs.readFileSync(configPath, "utf8");
     const config = JSON.parse(rawData);
     return config.port;
   } catch (error) {
     console.error("Failed to get server port:", error);
-    throw error;  // This will send the error back to the renderer
+    throw error; // This will send the error back to the renderer
+  }
+});
+
+ipcMain.handle("get-server-port", (event) => {
+  try {
+    // Using app.getAppPath() to get the root directory of the app
+    const appRoot = app.getAppPath();
+
+    // Construct the path to the config file
+    const configPath = path.join(appRoot, "backend", "config.json");
+
+    const rawData = fs.readFileSync(configPath, "utf8");
+    const config = JSON.parse(rawData);
+    return config.port;
+  } catch (error) {
+    console.error("Failed to get server port:", error);
+    throw error; // This will send the error back to the renderer
   }
 });
