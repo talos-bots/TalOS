4a7efeb1f0222c34b0bbb4b75ffd1d69b5e187ae
thesentinel2615 <nevyn202@outlook.com>
2023-08-19 20:19:32 -0500

    fix card imports

--- src/api/extrasapi/index.tsx
+++ src/api/extrasapi/index.tsx
@@ -6,70 +6,84 @@ import extract from 'png-chunks-extract';
 // @ts-ignore
 import PNGtext from 'png-chunk-text';
 
-export const importTavernCharacter = (
-    file: File
-): Promise<Construct> => {
-    console.log("Importing Tavern Card");
-    const reader = new FileReader();
-    let base64Image: string = '';
-    reader.onload = async (event) => {
-        console.log("Reading Tavern Card");
-        if (event.target && typeof event.target.result === 'string') {
-            base64Image = event.target.result;
-        }
-    };
-    reader.readAsDataURL(file);
+export const importTavernCharacter = (file: File): Promise<Construct> => {
     return new Promise((resolve, reject) => {
-        console.log("Parsing Tavern Card");
-        exifr.parse(file).then( async (metadata) => {
-            console.log("Tavern Card Parsed");
-            if (metadata) {
-                const isCardV2 = metadata.chara;
-                if (isCardV2) {
-                    console.log("New Tavern Card Detected");
-                    const decodedString = atob(metadata.chara);
-                    const cardSpec = JSON.parse(decodedString);
-                    let characterData;
-                    if (cardSpec?.data !== undefined) {
-                        characterData = cardSpec.data;
-                    }else{
-                        console.log("Old Tavern Card Detected");
-                        characterData = cardSpec;
-                    }
-                    characterData.avatar = base64Image;
-                    const construct = await processCharacterData(characterData);
-                    resolve(construct);
+        const reader = new FileReader();
+
+        // Create a new promise to read the file and get the base64 data
+        const readFilePromise = new Promise<string>((resolve, reject) => {
+            reader.onload = (event) => {
+                if (event.target && typeof event.target.result === 'string') {
+                    resolve(event.target.result);  // resolve the promise with the base64 string
                 } else {
-                    tryParseOldCard(file, base64Image).then((construct) => {
-                        if (construct) {
-                            resolve(construct);
-                        } else {
-                            reject();
-                        }
-                    });
+                    reject(new Error("File reading failed"));
                 }
-            }
-        }).catch((error) => {
-            console.log("Tavern Card Parse Failed", error);
-            tryParseOldCard(file, base64Image).then((construct) => {
-                console.log("Exif parser failure. Trying old parser");
-                if (construct) {
-                    resolve(construct);
-                } else {
-                    reject();
+            };
+            reader.onerror = (error) => reject(error);
+            reader.readAsDataURL(file);
+        });
+
+        readFilePromise.then(base64Image => {
+            console.log("Parsing Tavern Card");
+            exifr.parse(file).then(async (metadata) => {
+                console.log("Tavern Card Parsed");
+                if (metadata) {
+                    const isCardV2 = metadata.chara;
+                    if (isCardV2) {
+                        console.log("New Tavern Card Detected");
+                        const decodedString = atob(metadata.chara);
+                        const cardSpec = JSON.parse(decodedString);
+                        let characterData;
+                        if (cardSpec?.data !== undefined) {
+                            characterData = cardSpec.data;
+                        } else {
+                            console.log("Old Tavern Card Detected");
+                            characterData = cardSpec;
+                        }
+                        characterData.avatar = base64Image;
+                        console.log("Image64", base64Image);
+                        console.log(characterData);
+                        const construct = await processCharacterData(characterData, base64Image);
+                        resolve(construct);
+                    } else {
+                        tryParseOldCard(file, base64Image).then((construct) => {
+                            if (construct) {
+                                resolve(construct);
+                            } else {
+                                reject();
+                            }
+                        });
+                    }
                 }
             }).catch((error) => {
-                console.log("Old Tavern Card Parse Failed", error);
-                reject();
+                console.log("Tavern Card Parse Failed", error);
+                tryParseOldCard(file, base64Image).then((construct) => {
+                    console.log("Exif parser failure. Trying old parser");
+                    if (construct) {
+                        resolve(construct);
+                    } else {
+                        reject();
+                    }
+                }).catch((error) => {
+                    console.log("Old Tavern Card Parse Failed", error);
+                    reject();
+                });
             });
+        }).catch(error => {
+            console.log("Failed to read the file as base64", error);
+            reject(error);
         });
     });
 };
 
-async function processCharacterData(characterData: any): Promise<Construct> {
+
+async function processCharacterData(characterData: any, avatar64: string): Promise<Construct> {
     console.log(characterData);
     const construct = new Construct();
     construct.avatar = characterData.avatar;
+    if (avatar64) {
+        construct.avatar = avatar64;
+    }
     construct.name = (characterData.name || '').replaceAll('\r', '');
     construct.background = (characterData.scenario || '').replaceAll('\r', '');
 
@@ -85,6 +99,7 @@ async function processCharacterData(characterData: any): Promise<Construct> {
         construct.addGreeting(characterData.first_mes.replaceAll('\r', ''));
     }
 
+    console.log(construct);
     await saveNewConstruct(construct);
     return construct;
 }
@@ -113,8 +128,7 @@ const tryParseOldCard = (file: File, base64Image: string): Promise<Construct | v
                     try {
                         let _json = JSON.parse(decodedString);
                         _json.avatar = base64Image;
-                        console.log(_json);
-                        const construct = await processCharacterData(_json);
+                        const construct = await processCharacterData(_json, base64Image);
                         resolve(construct);
                     } catch (error) {
                         console.error('Error parsing JSON:', error);
