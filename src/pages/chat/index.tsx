79da878629b824127a18ec4d57989dbeb3ab7ecd
thesentinel2615 <nevyn202@outlook.com>
2023-08-22 12:21:31 -0500

    prep for working chat page

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -2,22 +2,25 @@ import { FunctionComponent, useState, useEffect } from "react";
 import InputGroup from "@/components/chat-page/ChatInput";
 
 const ChatPage: React.FC = () => {
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
+	useEffect(() => {
+		document.body.style.overflow = "hidden";
 
-    return () => {
-      document.body.style.overflow = "auto"; // i might be royally retarded but uhh yeah this is the only way i could disable scrolling while keeping the vh calc
-    };
-  }, []);
+		return () => {
+		document.body.style.overflow = "auto"; // i might be royally retarded but uhh yeah this is the only way i could disable scrolling while keeping the vh calc
+		};
+	}, []);
 
-  
-  return (
-    <div className={`relative w-full flex flex-col items-center justify-center themed-root`}>
-      <div className="box-border w-[1440px] h-[calc(100vh-70px)] shrink-0 flex flex-col pt-[740px] px-[145px] pb-[47px] items-start justify-end gap-[10px]">
-        <InputGroup />
-      </div>
-    </div>
-  );
+	const handleMessageSend = async (message: string) => {
+		console.log(message);
+	};
+
+	return (
+		<div className={`relative w-full flex flex-col items-center justify-center themed-root`}>
+			<div className="box-border w-[1440px] h-[calc(100vh-70px)] shrink-0 flex flex-col pt-[740px] px-[145px] pb-[47px] items-start justify-end gap-[10px]">
+				<InputGroup sendMessage={handleMessageSend}/>
+			</div>
+		</div>
+	);
 };
 
 export default ChatPage;
