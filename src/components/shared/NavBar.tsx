bd4880fd8f3547f3b6d37fb0114a8514afa29bba
thesentinel2615 <nevyn202@outlook.com>
2023-08-29 21:28:03 -0500

    theme revamp!!!

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -8,25 +8,25 @@ const NavBar: React.FC = () => {
   const location = useLocation();
 
   return (
-  <nav className={`sm:px-16 px-6 w-full flex items-center justify-between py-5 fixed top-0 z-20 bg-black bg-opacity-50 backdrop-blur-xl`}>
+  <nav className={`sm:px-16 px-6 w-full flex items-center justify-between py-5 fixed top-0 z-20 bg-theme-box bg-opacity-50 backdrop-blur-xl border-b-theme-border-width border-b-theme-border theme-border-style`}>
     <div className="flex items-center gap-2">
-      <p className="text-white text-[18px] font-bold">Construct<span className="text-theme-flavor-text">OS</span> - AI Agent Manager</p>
+      <p className="text-theme-text text-[18px] font-bold">Construct<span className="text-theme-flavor-text">OS</span> - AI Agent Manager</p>
     </div>
     <div className="hidden md:flex gap-5">
-      <NavLink to="/" title="Home" className={`p-1 transition-all duration-125 hover:opacity-25`} >
-        <HomeIcon style={location.pathname === "/" ? { color: 'white' } : { color: '#c2c2c2' }}/>
+      <NavLink to="/" title="Home" className={`p-1 transition-all duration-125 hover:opacity-50`} >
+        <HomeIcon style={location.pathname === "/" ? { color: 'text-theme-italic' } : { color: 'text-theme-text' }}/>
       </NavLink>
-      <NavLink to="/chat" title="Chat" className={`p-1 transition-all duration-125 hover:opacity-25`} >
-        <MessageCircle style={location.pathname === "/chat" ? { color: 'white' } : { color: '#c2c2c2' }}/>
+      <NavLink to="/chat" title="Chat" className={`p-1 transition-all duration-125 hover:opacity-50`} >
+        <MessageCircle style={location.pathname === "/chat" ? { color: 'text-theme-italic' } : { color: 'text-theme-text' }}/>
       </NavLink>
-      <NavLink to="/constructs" title="Constructs" className={`p-1 transition-all duration-125 hover:opacity-25`} >
-        <Users style={location.pathname === "/constructs" ? { color: 'white' } : { color: '#c2c2c2' }}/>
+      <NavLink to="/constructs" title="Constructs" className={`p-1 transition-all duration-125 hover:opacity-50`} >
+        <Users style={location.pathname === "/constructs" ? { color: 'text-theme-italic' } : { color: 'text-theme-text' }}/>
       </NavLink>
-      <NavLink to="/discord" title="Discord Bot" className={`p-1 transition-all duration-125 hover:opacity-25`} >
-        <Bot style={location.pathname === "/discord" ? { color: 'white' } : { color: '#c2c2c2' }}/>
+      <NavLink to="/discord" title="Discord Bot" className={`p-1 transition-all duration-125 hover:opacity-50`} >
+        <Bot style={location.pathname === "/discord" ? { color: 'text-theme-italic' } : { color: 'text-theme-text' }}/>
       </NavLink>
-      <NavLink to="/settings" title="Settings" className={`p-1 transition-all duration-125 hover:opacity-25`}>
-        <Cog style={location.pathname === "/settings" ? { color: 'white' } : { color: '#c2c2c2' }}/>
+      <NavLink to="/settings" title="Settings" className={`p-1 transition-all duration-125 hover:opacity-50`}>
+        <Cog style={location.pathname === "/settings" ? { color: 'text-theme-italic' } : { color: 'text-theme-text' }}/>
       </NavLink>
       {/* <HashLink to="/nodes" title="Nodes" className={`p-1 transition-all duration-125 hover:opacity-25`}>
         <Sparkles style={location.hash === "/nodes" ? { color: 'white' } : { color: '#c2c2c2' }} />
