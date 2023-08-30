bceebdfad5316ada01303f139f021b6f34482355
thesentinel2615 <nevyn202@outlook.com>
2023-08-29 23:34:21 -0500

    add in better user spacing

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -28,8 +28,8 @@ const UserPage = () => {
     if(isLoading) return (<Loading />);
 
     return (
-        <div className="w-95vw h-[calc(100vh-70px)] gap-4 m-auto grow-0 overflow-y-auto overflow-x-hidden">
-            <div className="grid grid-cols-3 m-auto w-full h-15/16 gap-2 mt-8">
+        <div className="w-95vw h-[calc(100vh-70px)] flex flex-col justify-center items-center gap-8 m-auto grow-0 overflow-y-auto overflow-x-hidden">
+            <div className="grid grid-cols-3 m-auto w-full h-11/12 gap-2">
                 <div className="col-span-1 themed-root gap-2 h-full overflow-y-auto flex flex-col">
                     <h3 className="text-xl font-semibold">User Profiles</h3>
                     {Array.isArray(users) && users.map((user: User) => {
