b986fe7093bd39266629aba58015e77aa3b0321a
thesentinel2615 <nevyn202@outlook.com>
2023-09-10 19:33:35 -0500

    fix a space issue

--- dist-electron/main/index.js
+++ dist-electron/main/index.js
@@ -1126,7 +1126,7 @@ function assemblePromptFromLog(data, messagesToInclude = 25) {
             }
           }
         }
-        prompt += `${messages[i].user}: ${messages[i].text.trim()} ${captionText}
+        prompt += `${messages[i].user}: ${messages[i].text.trim()}${captionText}
 `;
       }
     }
--- electron/main/helpers/helpers.ts
+++ electron/main/helpers/helpers.ts
@@ -125,7 +125,7 @@ export function assemblePromptFromLog(data: any, messagesToInclude: number = 25)
 						}
 					}
 				}
-                prompt += `${messages[i].user}: ${messages[i].text.trim()} ${captionText}\n`;
+                prompt += `${messages[i].user}: ${messages[i].text.trim()}${captionText}\n`;
             }
 		}
 	}
