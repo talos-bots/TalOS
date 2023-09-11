147c92ee19f7c31cc66f74936d3457e7c4ad1a47
thesentinel2615 <nevyn202@outlook.com>
2023-09-11 16:38:01 -0500

    Update index.tsx

--- src/components/llm-panel/palm-panel/index.tsx
+++ src/components/llm-panel/palm-panel/index.tsx
@@ -77,12 +77,12 @@ const PaLMPanel = (props: PaLMPanelProps) => {
         });
         const modelList: PaLMModel[] = [];
         models?.data.models.forEach((model: PaLMModel) => {
-            if(model.supportedGenerationMethods.includes('generateText')){
+            if(model?.supportedGenerationMethods.includes('generateText')){
                 modelList.push(model);
             }
         });
         setModels(modelList);
-        let foundModel = modelList.find((model) => model.name === selectedModel);
+        let foundModel = modelList.find((model) => model?.name === selectedModel);
         if(foundModel === undefined){
             foundModel = modelList[0];
             setSelectedModelObject(foundModel);
@@ -91,7 +91,7 @@ const PaLMPanel = (props: PaLMPanelProps) => {
     
     useEffect(() => {
         if(selectedModel === '') return;
-        let foundModel = models.find((model) => model.name === selectedModel);
+        let foundModel = models.find((model) => model?.name === selectedModel);
         if(foundModel === undefined){
             foundModel = models[0];
             setSelectedModelObject(foundModel);
@@ -144,25 +144,25 @@ const PaLMPanel = (props: PaLMPanelProps) => {
                 {selectedModelObject !== null ? (
                     <div className="flex flex-col gap-1 themed-box">
                         <label className="text-theme-text text-shadow-xl font-semibold">Name</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.name}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.name}</label>
                         <label className="text-theme-text text-shadow-xl font-semibold">Display Name</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.displayName}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.displayName}</label>
                         <label className="text-theme-text text-shadow-xl font-semibold">Input Token Limit</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.inputTokenLimit}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.inputTokenLimit}</label>
                         <label className="text-theme-text text-shadow-xl font-semibold">Output Token Limit</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.outputTokenLimit}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.outputTokenLimit}</label>
                         <label className="text-theme-text text-shadow-xl font-semibold">Supported Generation Methods</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.supportedGenerationMethods.join(', ')}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.supportedGenerationMethods.join(', ')}</label>
                         <label className="text-theme-text text-shadow-xl font-semibold">Temperature</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.temperature}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.temperature}</label>
                         <label className="text-theme-text text-shadow-xl font-semibold">Top K</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.topK}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.topK}</label>
                         <label className="text-theme-text text-shadow-xl font-semibold">Top P</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.topP}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.topP}</label>
                         <label className="text-theme-text text-shadow-xl font-semibold">Version</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.version}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.version}</label>
                         <label className="text-theme-text text-shadow-xl font-semibold">Description</label>
-                        <label className="text-theme-text text-shadow-xl">{selectedModelObject.description}</label>
+                        <label className="text-theme-text text-shadow-xl">{selectedModelObject?.description}</label>
                     </div>
                 ) : null}
                 <button className="themed-button-pos w-full" onClick={() => {setPalmModel(selectedModel);}}>Save</button>
