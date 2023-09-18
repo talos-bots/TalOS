d5724f9d52f3868d1a991aa7d27981bfbab271d5
thesentinel2615 <nevyn202@outlook.com>
2023-09-18 15:05:13 -0500

    add in emotes

--- src/pages/chat/chat-input/index.tsx
+++ src/pages/chat/chat-input/index.tsx
@@ -1,5 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { PlusCircle, SendHorizonal } from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
+import { PlusCircle, SendHorizonal, Smartphone, SmilePlus } from "lucide-react";
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
+
 interface InputGroupProps {
     sendMessage: (message: string, files?: File[]) => void;
 }
@@ -7,7 +10,9 @@ const InputGroup = (props: InputGroupProps) => {
   	const { sendMessage } = props;
 	const [message, setMessage] = useState<string>("");
 	const [attachments, setAttachments] = useState<File[]>([]);
-
+	const [showPicker, setShowPicker] = useState<boolean>(false);
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+	
 	const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const files = e.target.files;
 		if (files) {
@@ -16,20 +21,49 @@ const InputGroup = (props: InputGroupProps) => {
 		}
 	};
 
+	const addEmoji = (emoji: any) => {
+		const start = (textAreaRef.current?.selectionStart) || 0;
+		const end = (textAreaRef.current?.selectionEnd) || 0;
+		const currentMessage = message;
+		const before = currentMessage.slice(0, start);
+		const after = currentMessage.slice(end);
+		const text = before + emoji.native + after;
+		
+		setMessage(text);
+		setShowPicker(false);
+		
+		setTimeout(() => {
+			textAreaRef.current?.setSelectionRange(start + emoji.native.length, start + emoji.native.length);
+			textAreaRef.current?.focus();
+		}, 0);
+	};
+
 	return (
+		<>
+		{showPicker && 
+			<div className="absolute z-100 bottom-20">
+				<Picker data={data} onSelect={addEmoji} className="" />
+			</div>
+		}
 		<div className="chat-input-bar slide-in-bottom">
-			<input 
-				type="file" 
-				onChange={(event) => handleFileUpload(event)} 
-				style={{ display: 'none' }} 
-				id="file-upload" 
-				multiple
-			/>
-			<label htmlFor="file-upload" className={`message-button`}>
-				<PlusCircle size={'1.5rem'}/>
-			</label>
+			<div className="flex flex-row gap-2">
+				<input 
+					type="file" 
+					onChange={(event) => handleFileUpload(event)} 
+					style={{ display: 'none' }} 
+					id="file-upload" 
+					multiple
+				/>
+				<label htmlFor="file-upload" className={`message-button`} title="Upload attachments">
+					<PlusCircle size={'1.5rem'}/>
+				</label>
+				<button className={`message-button`} onClick={() => setShowPicker((prev) => !prev)} title="Pick an Emoji">
+					<SmilePlus size={'1.5rem'}/>
+				</button>
+			</div>
 			<textarea
-				autoComplete="off"
+				ref={textAreaRef}
+				autoComplete="on"
 				className="border-none font-inter text-lg bg-transparent flex-grow h-10 overflow-hidden py-0 px-5 box-border items-start justify-center max-h-8 resize-none overflow-y-auto"
 				value={message}
 				placeholder="Type your message..."
@@ -47,10 +81,11 @@ const InputGroup = (props: InputGroupProps) => {
 					}
 				}}
 			/>
-			<button className={`message-button`} onClick={() => { sendMessage(message, attachments); setMessage(''); setAttachments([]); }}>
+			<button className={`message-button`} onClick={() => { sendMessage(message, attachments); setMessage(''); setAttachments([]); }} title="Send message">
 				<SendHorizonal size={'1.5rem'}/>
 			</button>
 		</div>
+		</>
 	);
 };
 
