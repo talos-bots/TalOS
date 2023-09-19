4885bbc2bedf9f47457a53615074248acafa04ab
thesentinel2615 <nevyn202@outlook.com>
2023-09-19 17:18:26 -0500

    fix another oopsie

--- src/components/confirm-modal/index.tsx
+++ src/components/confirm-modal/index.tsx
@@ -13,7 +13,7 @@ const ConfirmModal = ({ message, note }: ConfirmModalProps) => {
     isVisible && (
       <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-50 flex justify-center items-center">
         <div className="themed-root flex flex-col justify-center items-center p-4 rounded-lg backdrop-blur-theme-blur">
-          <h1 className="text-2xl">Are you sure you want to {message}?</h1>
+          <h1 className="text-2xl">{message}</h1>
           {note && <p className="text-sm">{note}</p>}
           <div className="flex flex-row gap-4 mt-4">
             <button
