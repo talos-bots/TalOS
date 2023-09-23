27322611cd8f7a8ef6a4ff456679b92ccd29aeef
thesentinel2615 <nevyn202@outlook.com>
2023-09-23 15:40:10 -0500

    fix attachment page

--- src/pages/attachments/attachment/index.tsx
+++ src/pages/attachments/attachment/index.tsx
@@ -15,7 +15,7 @@ const ImageAttachmentComponent = (props: AttachmentComponentProps) => {
         <div className={`col-span-1 w-full h-full themed-root flex flex-col justify-center items-center gap-2 flex-grow-0 aspect-square ${isDeleted? 'pop-out': 'slide-in-left'}`}>
             <p className="text-center font-semibold">{attachment.name}</p>
             <img src={getAttachmentData(attachment)} className="w-full h-full object-contain rounded-theme-border-radius bg-theme-box border-theme-border-width border-theme-border"/>
-            <p>
+            <p className="w-full break-words">
                 {assembleMetadata(attachment)}
             </p>
             <div className="grid grid-cols-2 gap-2 justify-center">
