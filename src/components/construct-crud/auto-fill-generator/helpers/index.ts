1e7d1ba1bb35ba311c76455657e707ea4dc881c7
thesentinel2615 <nevyn202@outlook.com>
2023-09-24 12:53:24 -0500

    add in some prompts

--- src/components/construct-crud/auto-fill-generator/helpers/index.ts
+++ src/components/construct-crud/auto-fill-generator/helpers/index.ts
@@ -56,6 +56,26 @@ Write the dialogue for a character in the format of a conversation, with the use
 
 `
 
+const greetingPrompt = `
+{{char}}: *she looks at you pensively* Hello, {{user}}. *her face turns into a smile* Not quite sure where you are?
+
+{{char}}: *Pardofelis yawned and stretched, gracefully arching her back as she jumped off a shiny pile of trinkets with a cheeky grin. Her cat-like eyes sparkled like precious gems as she stumbled toward you, the various trinkets hanging from her outfit jingling gently with each step.* Ah, hello there, boss! You've just stumbled upon the most purrfect treasure trove there is, run by yours truly! *She strode towards you, her tail swishing excitedly while her cat ears remained perked up with curiosity.* Welcome to Pardofelis' Emporium of Shiny things! *Her tail swished energetically as she took a step back and observed you closely.* So tell me, boss—what're ya lookin' for today? Got anything special in mind? Or maybe you're here to sell a treasure? *Her eyes twinkled mischievously as she tugged at one braid, eagerly awaiting your response.*
+
+{{char}}: Aruna entered her hut, where {{user}} was resting. "Whew, finally awake! You were out for so long I thought you were dead!" She said before giggling mischievously. Light rays pierced through gaps between the leaves that made up the hut's ceiling, reflecting off of Aruna's abs and toned thighs as she looked down at {{user}}. "Alright, here's the deal, knight of the Empire," she squatted down, looking straight into your eyes with a smug expression. "The Bakumo tribe values strong people. You look strong, so instead of leaving your unconscious body as food for the forest's creatures, I, Aruna, brought you here and treated you." She spoke as though she had done that out of a sense of justice, when in reality, Aruna only brought {{user}} to her hut because she found {{user}} cute. "So to repay my kindness, you're gonna become a member of the Bakumo tribe, as well as my underling~!" She said enthusiastically before regaining her composure and putting on a fake air of authority. "Ahem, anyway, you don't have a choice. The only way for you to stay here is follow me. If you refuse my offer, the matriarch will personally kick your ass back into the forest hehe." Extending her hand to {{user}}, Aruna spoke again, "Get up, sleepyhead. We're gonna go see my mother so she can give us a mission to prove your worth as the newest Bakumo warrior."
+
+{{char}}: *She curtsies gently before you, the folds of her crimson gown cascading to the floor.* Greetings, dear friend, welcome to my Golden Courtyard. Though this old bar has seen better days, it is still cozy and intimate enough for a drink between two souls seeking company. The wine here is as velvety as my voice is, a perfect elixir to ease the worries of this world. *Eden leads you through a doorway draped with threads of golden silk. The private parlor beyond was breath-taking. Golden walls rise like a sun-kissed dawn. A crackling fireplace of marble and onyx warms the space. In the center, a plush velvet sofa.* Please, make yourself comfortable, friend. *She strides to the far shelf, the layered skirts of her gowns fluttering with each step. She grasps an ancient bottle of red vintage, before uncorking it with practiced ease. The aroma wafts up. Eden pours two glasses and offers you one of them.* Drink with me tonight, and we shall fill this silence.
+
+{extracontext}
+
+### Instruction:
+Use actions and dialogue, either in *action text* or novel formatting to craft an introduction for a character.
+{request}
+
+{context}
+
+### Response:
+
+`
 const thoughtPrompt = `
 {extracontext}
 
@@ -120,6 +140,9 @@ export async function getReturnValue(type: requestTypes, field: string, suggesti
     if(field === "thoughtpattern"){
         prompt = thoughtPrompt;
     }
+    if(field === "greetings"){
+        prompt = greetingPrompt;
+    }
     let context = "";
     if(currentValues && currentValues !== null && useExisting){
         let constructInfo = assembleConstructPrompt(currentValues);
@@ -135,6 +158,9 @@ export async function getReturnValue(type: requestTypes, field: string, suggesti
         context += extraContext;
     }
     prompt = prompt.replace("{request}", suggestion || "").replace("{context}", context).replace("{extracontext}", extraContext || "").replace("{field}", field || "");
+    if(currentValues?.name !== undefined && currentValues?.name.length > 1 && useExisting){
+        prompt = prompt.replaceAll('character', currentValues.name);
+    }
     let result = await generateText(prompt);
     if(result?.results[0].length > 1){
         return result.results[0];
